9544264ac3bb0fd25017b6bd2f0266d090fd1ebc
scy6500 <55119006+scy6500@users.noreply.github.com>
2021-05-06 18:50:57 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # devops-eng-training
-## TODO(everyone): README.md 완성하기
+### 1. 프로젝트 소개
+해당 프로젝트는 모두의연구소 풀잎스쿨 MLOps 스터디에서 진행한 DevOps 실습입니다.  
+Flask를 사용하여 웹서버를 구현하였으며, GET Method를 통해 사칙연산을 할 수 있습니다.  
+unittest/functions_test.py에서는 사칙연산 함수를 테스트 해볼 수 있고, integration/app_test.pt에서는 웹서버의 상태를 확인할 수 있습니다.
+
+### 2. 사용법
+1. 웹서버 실행  
+   ```shell
+    python app.py
+    ```
+2. 서버에 요청 보내기  
+   ```shell
+    curl -X GET 'http://localhost:5000/add?num1=10&num2=2'
+    ```
+
+### 3. 테스트 방법
+1. 사칙연산 함수 테스트
+   ```shell
+     cd devops-eng-training/unittest
+     pytest
+   ```
+2. 서버 상태 테스트
+   ```shell
+     cd devops-eng-training/integration
+     pytest
+   ```
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from functions import add_function, subtract_function, multiply_function, division_function
 app = Flask(__name__)
 
 
@@ -13,7 +14,37 @@ def healthCheck():
 
 
 # TODO(everyone): GET 메소드로 더하기, 빼기, 곱하기, 나누기 함수 라우트 완성하기
+@app.route("/add", methods=["GET"])
+def add():
+    num1 = request.args.get("num1")
+    num2 = request.args.get("num2")
+    result = add_function(num1, num2)
+    return num1 + "+" + num2 + "=" + result
+
+
+@app.route("/subtract", methods=["GET"])
+def subtract():
+    num1 = request.args.get("num1")
+    num2 = request.args.get("num2")
+    result = subtract_function(num1, num2)
+    return num1 + "-" + num2 + "=" + result
+
+
+@app.route("/multiply", methods=["GET"])
+def multiply():
+    num1 = request.args.get("num1")
+    num2 = request.args.get("num2")
+    result = multiply_function(num1, num2)
+    return num1 + "*" + num2 + "=" + result
+
+
+@app.route("/division", methods=["GET"])
+def division():
+    num1 = request.args.get("num1")
+    num2 = request.args.get("num2")
+    result = division_function(num1, num2)
+    return num1 + "/" + num2 + "=" + result
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='80', debug=True)
+    app.run(host='0.0.0.0', port='5000', debug=True)
