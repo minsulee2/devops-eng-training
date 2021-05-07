ad79218eea26903b46e391cf7862753191ab636f
l-yohai <yohan9612@naver.com>
2021-05-07 17:39:54 +0900

    feat: add route four operation functions

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from functions import add, minus, multiply, divide
+
 app = Flask(__name__)
 
 
@@ -13,6 +15,28 @@ def healthCheck():
 
 
 # TODO(everyone): GET 메소드로 더하기, 빼기, 곱하기, 나누기 함수 라우트 완성하기
+@app.route("/add")
+def operate_add():
+    num1, num2 = request.args.get('num1'), request.args.get('num2')
+    return add(num1, num2)
+
+
+@app.route("/minus")
+def operate_minus():
+    num1, num2 = request.args.get('num1'), request.args.get('num2')
+    return minus(num1, num2)
+
+
+@app.route("/multiply")
+def operate_multiply():
+    num1, num2 = request.args.get('num1'), request.args.get('num2')
+    return multiply(num1, num2)
+
+
+@app.route("/divide")
+def operate_divide():
+    num1, num2 = request.args.get('num1'), request.args.get('num2')
+    return divide(num1, num2)
 
 
 if __name__ == '__main__':
