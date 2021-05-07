b90f4b7ca21e883ccfb20662a9166fdb2c1062b6
l-yohai <yohan9612@naver.com>
2021-05-07 17:39:33 +0900

    feat: four operations function define

--- functions.py
+++ functions.py
@@ -1,2 +1,16 @@
 # TODO(everyone): 더하기, 빼기, 곱하기, 나누기 함수 정의하기
 
+def add(a, b):
+    return str(int(a) + int(b))
+
+
+def minus(a, b):
+    return str(int(a) - int(b))
+
+
+def multiply(a, b):
+    return str(int(a) * int(b))
+
+
+def divide(a, b):
+    return str(int(a) / int(b))
