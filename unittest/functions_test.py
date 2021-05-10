055c4697c57c60b73a9cbad46f6fbd650f61d54a
l-yohai <yohan9612@naver.com>
2021-05-10 17:23:03 +0900

    feat: unittest for pytest

---
+++ unittest/functions_test.py
@@ -0,0 +1,24 @@
+# TODO(everyone): 더하기, 빼기, 곱하기, 나누기 함수 테스트 케이스 작성
+
+import pytest
+import os
+import sys
+sys.path.append(
+    os.path.dirname(os.path.dirname(__file__))
+)
+from functions import add, minus, multiply, divide
+
+def test_add():
+    assert add(1, 2) == 1 + 2
+
+
+def test_minus():
+    assert minus(5, 3) == 5 - 3
+
+
+def test_multiply():
+    assert multiply(4, 6) == 4 * 6
+
+
+def test_divide():
+    assert divide(8, 2) == 8 / 2
